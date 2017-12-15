e36c0dd5d1122ad6c9dfbcca86d08bad3ecb6a6d
Jordan Boggs <boggsjp@gmail.com>
2017-12-15 10:52:49 -0700

    initial work with moment.js and writing the table

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,24 +41,26 @@ $(document).on("click", "#submit-button", function(event) {
     "minutesAway": calculateMinutes(firstTrain, frequency)
   });
   localStorage.setItem("trainArray", JSON.stringify(trainArray));
+  drawTable();
 });
 
 /*
  * Function that calculates next arrival and time until next arrival
  * 
  * We'll definitely need moment.js for this
+ * Parse as HH:mm for 24-hour time
+ * e.g., moment(start, "HH:mm");
+ * 
+ * You can add time with the .add() method
+ * e.g., moment(start, "HH:mm").add(freq, 'm');
  */
 function calculateArrival(start, freq) {
-  let now = moment();
-  // let firstTrain = I need to figure out how to parse the input time
-  
-  let arrival = start + freq;
-
-  return arrival;
+  return moment(start, "HH:mm").add(freq, 'm');
 }
 
 function calculateMinutes(start, freq) {
-
+  let nextArrival = moment(start, "HH:mm").add(freq, 'm');
+  return moment(nextArrival).fromNow();
 }
 
  /*
@@ -66,4 +68,18 @@ function calculateMinutes(start, freq) {
  * 
  * We'll have to parse time data using moment.js, I think
  */
-
+function drawTable() {
+  for (let index=0; index < trainArray.length; index++) {
+    let current = trainArray[index]
+    
+    $("#train-table").append(`
+      <tr>
+        <td>${current.trainName}</td>
+        <td>${current.destination}</td>
+        <td>${current.frequency}</td>
+        <td>${current.nextArrival}</td>
+        <td>${current.minutesAway}</td>
+      </tr>
+    `)
+  }
+}
