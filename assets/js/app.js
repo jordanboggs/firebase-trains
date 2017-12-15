7ccfddf385cd59ce48cadc73b5f1c00b4684d6fd
Jordan Boggs <boggsjp@gmail.com>
2017-12-14 17:28:39 -0700

    initial pseudocode

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,28 @@
  * Jordan Boggs
  * (c) 2017 DU Coding Bootcamp
  */
+
+/* 
+ * Function to add train info to wherever we're saving it
+ * 
+ * For now, let's save everything to local storage
+ * 
+ * We will also want to validate the data in some way
+ */ 
+
+/*
+ * Function that pulls data to add to table
+ * 
+ * We'll have to parse time data using moment.js, I think
+ */
+
+/*
+ * Function that calculates next arrival and time until next arrival
+ * 
+ * We'll definitely need moment.js for this
+ */
+
+/*
+ * Function that puts all info onto the table
+ * 
+ */
