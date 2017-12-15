aaefdcebf5d80d50bc3d50ea6b2bb3bf59516505
Jordan Boggs <boggsjp@gmail.com>
2017-12-14 17:38:02 -0700

    I forgot about frequency in the form... don't judge me

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,27 +7,40 @@
  * (c) 2017 DU Coding Bootcamp
  */
 
+var trainArray = JSON.parse(localStorage.getItem("trainArray"));
+if (!Array.isArray(trainArray)) {
+  trainArray = [];
+}
+
 /* 
  * Function to add train info to wherever we're saving it
+ * 
+ * Form id is #add-train
+ * Train name is #train-name
+ * Destination is #destination
+ * Frequency is #frequency
+ * First train time is #first-train-time
  * 
  * For now, let's save everything to local storage
  * 
  * We will also want to validate the data in some way
  */ 
+function formSubmit() {
 
-/*
- * Function that pulls data to add to table
- * 
- * We'll have to parse time data using moment.js, I think
- */
+}
 
 /*
  * Function that calculates next arrival and time until next arrival
  * 
  * We'll definitely need moment.js for this
  */
+function calculateTimes(firstTime, ) {
 
-/*
- * Function that puts all info onto the table
+}
+
+ /*
+ * Function that pulls data to add to table (#train-table)
  * 
+ * We'll have to parse time data using moment.js, I think
  */
+
